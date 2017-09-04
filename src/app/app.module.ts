c14e19b9331eae854037fa59973cf4286b9cb3b5
disco-funk <phil_hudson@outlook.com>
2017-09-04 07:30:12 +0100

    (feat): add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { MeetingsComponent } from './meetings/meetings.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'events',
+    component: EventsComponent,
+    data: { title: 'Events' }
+  },
+  {
+    path: 'meetings',
+    component: MeetingsComponent,
+    data: { title: 'Meetings' }
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home' }
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    MeetingsComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
